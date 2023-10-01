c07661936fe5e021419b63a18f0f8fa5bd66cd91
NhatLe2002 <nhatlvmse160608@fpt.edu.vn>
2023-10-01 11:19:10 +0700

    First commit

--- LicenseReal/src/java/dto/Answer.java
+++ LicenseReal/src/java/dto/Answer.java
@@ -10,5 +10,74 @@ package dto;
  * @author Admin
  */
 public class Answer {
+    private int answerID;
+    private int questionID;
+    private String options;
+    private String answer;
+    private boolean isCorrect;
+
+    public Answer() {
+    }
+
+    public Answer(int answerID, int questionID, String options, boolean isCorrect) {
+        this.answerID = answerID;
+        this.questionID = questionID;
+        this.options = options;
+        this.isCorrect = isCorrect;
+    }
+
+    public Answer(int questionID, String options, String answer, boolean isCorrect) {
+        this.questionID = questionID;
+        this.options = options;
+        this.answer = answer;
+        this.isCorrect = isCorrect;
+    }
+
+    public int getAnswerID() {
+        return answerID;
+    }
+
+    public void setAnswerID(int answerID) {
+        this.answerID = answerID;
+    }
+
+    public int getQuestionID() {
+        return questionID;
+    }
+
+    public void setQuestionID(int questionID) {
+        this.questionID = questionID;
+    }
+
+    public String getOptions() {
+        return options;
+    }
+
+    public void setOptions(String options) {
+        this.options = options;
+    }
+
+    public boolean isIsCorrect() {
+        return isCorrect;
+    }
+
+    public void setIsCorrect(boolean isCorrect) {
+        this.isCorrect = isCorrect;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    @Override
+    public String toString() {
+        return "Answer{" + "answerID=" + answerID + ", questionID=" + questionID + ", options=" + options + ", answer=" + answer + ", isCorrect=" + isCorrect + '}';
+    }
+
+  
     
 }
--- LicenseReal/src/java/dto/Question.java
+++ LicenseReal/src/java/dto/Question.java
@@ -5,10 +5,97 @@
  */
 package dto;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Admin
  */
 public class Question {
-    
+    private int id;
+    private String question;
+    private String image;
+    private ArrayList<Answer> answer;
+    private boolean questionType;
+    private int topicID;
+
+    public Question() {
+    }
+
+    public Question(int id, String question, String image, boolean questionType, ArrayList<Answer> answer) {
+        this.id = id;
+        this.question = question;
+        this.image = image;
+        this.answer = answer;
+        this.questionType = questionType;
+    }
+
+    public Question(int id, String question, String image, ArrayList<Answer> answer, boolean questionType, int topicID) {
+        this.id = id;
+        this.question = question;
+        this.image = image;
+        this.answer = answer;
+        this.questionType = questionType;
+        this.topicID = topicID;
+    }
+
+    public Question(int topicID) {
+        this.topicID = topicID;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public ArrayList<Answer> getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(ArrayList<Answer> answer) {
+        this.answer = answer;
+    }
+
+
+    public boolean isQuestionType() {
+        return questionType;
+    }
+
+    public void setQuestionType(boolean questionType) {
+        this.questionType = questionType;
+    }
+
+    public int getTopicID() {
+        return topicID;
+    }
+
+    public void setTopicID(int topicID) {
+        this.topicID = topicID;
+    }
+
+    @Override
+    public String toString() {
+        return "Question{" + "id=" + id + ", question=" + question + ", image=" + image + ", answer=" + answer + ", questionType=" + questionType + ", topicID=" + topicID + '}';
+    }
+
+
 }
