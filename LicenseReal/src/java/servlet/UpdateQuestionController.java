ae5f9498dba5c88370fb64aff762b4f593af61a2
Vo Thanh Tuyen <132963766+thanhtuyen662002@users.noreply.github.com>
2023-10-02 20:51:41 +0700

    Update question

---
+++ LicenseReal/src/java/servlet/UpdateQuestionController.java
@@ -0,0 +1,40 @@
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author HP Pro
+ */
+@WebServlet(name = "UpdateQuestionController", urlPatterns = {"/UpdateQuestionController"})
+public class UpdateQuestionController extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
+
+}
---
+++ LicenseReal/web/updateQuestion.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : updateQuestion
+    Created on : 02-10-2023, 20:40:09
+    Author     : HP Pro
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+    </body>
+</html>
